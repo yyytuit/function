df00266c1cc9545fb2f760f06c11c07a3d659331
吉松寛道 <yyytuit@yahoo.co.jp>
2020-07-28 01:34:56 +0900

    2章全て

--- app/models/db_shohin.rb
+++ app/models/db_shohin.rb
@@ -60,4 +60,55 @@ class DbShohin < ApplicationRecord
     # SQLの直書
     find_by_sql("SELECT shohin_mei, hanbai_tanka, hanbai_tanka * 2 AS hanbai_tanka_x2 FROM db_shohins").as_json
   end
+
+  def self.hikaku
+    # 比較演算
+    # Railsの書き方
+    select('shohin_mei, shohin_bunrui, hanbai_tanka').where.not(hanbai_tanka: 1000).as_json
+    # SQLの直書
+    find_by_sql("SELECT shohin_mei, shohin_bunrui, hanbai_tanka FROM db_shohins  WHERE hanbai_tanka <> 1000").as_json
+  end
+
+  def self.where_kesan
+    # WHERE区の条件式に計算式を書く
+    # Railsの書き方
+    select('shohin_mei, hanbai_tanka, shiire_tanka').where("hanbai_tanka - shiire_tanka >= 500").as_json
+    # SQLの直書
+    find_by_sql("SELECT shohin_mei, hanbai_tanka, shiire_tanka FROM db_shohins  WHERE hanbai_tanka - shiire_tanka >= 500").as_json
+  end
+
+  def self.nil
+    # WHERE区の条件式に計算式を書く
+    # SQLの直書
+    # find_by_sql("SELECT * FROM db_shohins  WHERE shiire_tanka = nil").as_json
+
+    # = nilは上手く行かないので、IS NULLをつかう
+    # Railsの書き方
+    where.(shiire_tanka: nil).as_json
+    # SQLの直書
+    find_by_sql("SELECT * FROM db_shohins  WHERE shiire_tanka IS NULL").as_json
+  end
+
+  def self.not_nil
+    # Railsの書き方
+    where.not(shiire_tanka: nil).as_json
+    # SQLの直書
+    find_by_sql("SELECT * FROM db_shohins  WHERE shiire_tanka IS NOT NULL").as_json
+  end
+
+  def self.and_or
+    # AND演算子とOR演算子
+    # AND
+    # Railsの書き方
+    where("shohin_bunrui =? AND hanbai_tanka >=? ", 'キッチン用品', 3000).as_json
+    where(shohin_bunrui: 'キッチン用品').where("hanbai_tanka >= 3000").as_json
+    # SQLの直書
+    find_by_sql("SELECT * FROM db_shohins  WHERE shohin_bunrui = 'キッチン用品' AND hanbai_tanka >=3000").as_json
+    # OR
+    # Railsの書き方
+    DbShohin.where("shohin_bunrui =? OR hanbai_tanka >=? ", 'キッチン用品', 3000).as_json
+    DbShohin.where("shohin_bunrui = 'キッチン用品'").or(DbShohin.where("hanbai_tanka >= 3000")).as_json
+    # # SQLの直書
+    find_by_sql("SELECT * FROM db_shohins  WHERE shohin_bunrui = 'キッチン用品' OR hanbai_tanka >=3000").as_json
+  end
 end
